db724a40e7f465502092575c384ef4e8bc27320e
Abigail Antrich <Abigail_Antrich@baylor.edu>
2019-02-27 11:55:31 -0600

    Format petList into table, notworking

--- src/main/js/petList.js
+++ src/main/js/petList.js
@@ -22,12 +22,24 @@ class PetList extends React.Component {
     render() {
         return (
         <div>
-            <p> this is a test </p>
-            {this.state.pets.map(pet => (
-              <div key = {pet.name} className="pet">
-                  <p>{pet.name}</p>
-              </div>
-            ))}
+            <h1> Pet Profile </h1>
+            <table style="width:80%">
+                <thead>
+                    <tr>
+                        <th>Pet Name</th>
+                        <th>Pet Type</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {this.state.pets.map(pet => (
+                  <tr key = {pet.name} className="pet">
+                      <td>{pet.name}</td>
+                      <td>{pet.type}</td>
+                  </tr>
+                ))}
+                </tbody>
+            </table>
+
         </div>
         );
       }
