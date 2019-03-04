963e67d25b3619eb7b91320482f41bc366e8095c
Yunzhe Liu <yunzhe_liu@baylor.edu>
2019-03-03 22:10:31 -0600

    add sitter endpoint

--- src/main/java/petfinder/site/common/user/sitter/SitterAvailabilityDao.java
+++ src/main/java/petfinder/site/common/user/sitter/SitterAvailabilityDao.java
@@ -1,4 +1,25 @@
 package petfinder.site.common.user.sitter;
 
+import alloy.elasticsearch.ElasticSearchClientProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import petfinder.site.elasticsearch.SitterAvailabilityRepository;
+
+import java.util.Optional;
+
+@Repository
 public class SitterAvailabilityDao {
+    @Autowired
+    private SitterAvailabilityRepository sitterAvailabilityRepository;
+
+    @Autowired
+    private ElasticSearchClientProvider elasticSearchClientProvider;
+
+    public Optional<SitterAvailabilityDto> findAvailability(String id) {
+        return sitterAvailabilityRepository.find(id);
+    }
+
+    public void save(SitterAvailabilityDto sitterAvailabilityDto) {
+        sitterAvailabilityRepository.save(sitterAvailabilityDto);
+    }
 }
---
+++ src/main/java/petfinder/site/endpoint/SitterEndpoint.java
@@ -0,0 +1,46 @@
+package petfinder.site.endpoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import petfinder.site.common.user.sitter.SitterAvailabilityDto;
+import petfinder.site.common.user.sitter.SitterService;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/sitters")
+public class SitterEndpoint {
+
+    @Autowired
+    private SitterService sitterService;
+    private SitterAvailabilityDto sitterAvailabilityDto;
+
+    // Take an id, and look up the corresponding pet
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public Optional<SitterAvailabilityDto> getAvailability(@PathVariable("id") String id) {
+        return sitterService.findAvailability(id);
+    }
+
+    // Take a JSON representation of a Pet and save it to Elasticsearch
+    @PostMapping(value = "")
+    public SitterAvailabilityDto savePet(@RequestBody SitterAvailabilityDto sitterAvailabilityDto) {
+        System.out.println(sitterAvailabilityDto.getPrincipal());
+        System.out.println(sitterAvailabilityDto.getAvailability());
+        sitterService.save(sitterAvailabilityDto);
+        return sitterAvailabilityDto;
+    }
+
+
+    @PostMapping(value = "/edit_availability")
+    public SitterAvailabilityDto editProfile(@RequestBody SitterAvailabilityDto sitterAvailabilityDto) {
+        System.out.println("updating" + sitterAvailabilityDto.getPrincipal());
+        return sitterService.update(sitterAvailabilityDto);
+    }
+}
