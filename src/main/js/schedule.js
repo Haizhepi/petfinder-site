7dbd6b7f8f877e7c33faf3791791c193e05d789a
Yunzhe Liu <yunzhe_liu@baylor.edu>
2019-03-24 17:06:51 -0500

    date picker css not working

--- src/main/js/schedule.js
+++ src/main/js/schedule.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom';
+import DatePicker from 'react-datepicker';
 import * as Validation from 'js/alloy/utils/validation';
 import * as Bessemer from 'js/alloy/bessemer/components';
 import * as ReduxForm from 'redux-form';
 import {connect} from 'react-redux';
 import * as Users from 'js/users';
 import 'styles/main.scss';
+//import 'react-datepicker/dist/react-datepicker.css';
 
 
 class AvailabilityForm extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDate: new Date()
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(date) {
+        this.setState({
+            startDate: date
+        });
+    }
     //Defines the on submit behavior
     onSubmit = (form) => {
         let avail = {
@@ -32,12 +48,24 @@ class AvailabilityForm extends React.Component {
         }
 
         return (
+            <div>
+
             <form name="form" onSubmit={handleSubmit(form => this.onSubmit(form))}>
                 <Bessemer.Field name="availability" friendlyName="Enter availability"/>
                 <div className="wrapper">
+                    <DatePicker
+                        selected={this.state.startDate}
+                        onChange={this.handleChange}
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={15}
+                        dateFormat="MMMM d, yyyy h:mm aa"
+                        timeCaption="time"
+                    />
                     <Bessemer.Button className="buttonType1" loading={submitting}>Confirm</Bessemer.Button>
                 </div>
             </form>
+            </div>
         );
     }
 }
